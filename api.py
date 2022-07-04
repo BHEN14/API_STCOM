83d31fb40a92cae76547e7c0cd6661fc0d5b39fd
Zeeshan <53146394+ElectraPro-pk@users.noreply.github.com>
2022-07-04 22:17:24 +0400

    check deploy

--- api.py
+++ api.py
@@ -1,22 +1,16 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask
 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
+from flask import Flask
+import pymongo
+
+
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
+db = pymongo.MongoClient("mongodb+srv://root:root@students.lvpkb.mongodb.net/?retryWrites=true&w=majority")
+coll = db["stcom"]
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
 def hello_world():
   return 'Hello World'
 
-# main driver function
-if __name__ == '__main__':
 
-  # run() method of Flask class runs the application
-  # on the local development server.
+if __name__ == '__main__':
   app.run()
